757f94211abe61708385af5fd46909033a019b30
smrutipatro <pratimapatro11@gmail.com>
2019-11-24 14:53:18 +0530

    changed file paths

--- add_train_and_valid_path.py
+++ add_train_and_valid_path.py
@@ -4,7 +4,9 @@ train_path = os.path.join("data","custom","train.txt")
 f = open(train_path,"w")
 for i in range(1,150):
 	image_name = "image"+str(i)+".png"
-	path_name = os.path.join("data","custom","images",image_name)
+	path_name = "data/"+"custom/"+"images/"+image_name
+	# path_name = os.path.join("data","custom","images",image_name)
+	# path_name1 = os.path.normpath(path_name)
 	f.write(path_name)
 	f.write("\n")
 f.close()
@@ -13,7 +15,8 @@ valid_path = os.path.join("data","custom","valid.txt")
 f1 = open(valid_path,"w")
 for i in range(150,184):
 	image_name = "image"+str(i)+".png"
-	path_name = os.path.join("data","custom","images",image_name)
+	path_name = "data/"+"custom/"+"images/"+image_name
+	#path_name = os.path.join(os.sep,"data","custom","images",image_name)
 	f1.write(path_name)
 	f1.write("\n")
 f1.close()
